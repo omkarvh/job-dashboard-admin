d183fac3fe2775a5157088561e2991af7dd58dfd
Omkar Hiremath <55254173+omkarvh@users.noreply.github.com>
2025-06-22 09:57:45 +0530

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://job-dashboard-backend-bdi6.onrender.com",
   withCredentials: true, // send cookies with request
 });
 
